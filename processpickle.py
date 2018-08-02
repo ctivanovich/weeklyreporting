916b76417f1aa18d127ecacff08f4b4f62645703
ctivanovich <ct.ivanovich@gmail.com>
2018-08-02 16:16:27 +0800

    Reportwriter complete, further optimizations

--- processpickle.py
+++
@@ -1,7 +0,0 @@
-import pickle
-import os
-
-os.getcwd()
-with open(os.getcwd()+'\\weekreport\\results.pickle', 'rb') as f:
-    results = pickle.load(f)
-results
--- reportwriter.py
+++ reportwriter.py
@@ -1,36 +1,33 @@
 import os
 import openpyxl
 import pickle
-from decimal import Decimal
 
 from fields import fields
 
 column = 'CI'
 name_col = 'E'
 
-pklpaths = [p for p in os.listdir() if '.pkl' in p]
+pklpaths = [p for p in os.listdir('../temp') if '.pkl' in p]
 regions = ['sh-lawson', 'bj-lawson', 'cq-lawson', 'wh-lawson', 'dl-lawson']
-template = '../report_template.xlsx'
+template = '../temp/report_template.xlsx'
 
-with open(pklpaths[3], 'rb') as f:
-    p = pickle.load(f)
-    wb = openpyxl.load_workbook(template)
-    sh = wb['weekly']
+for pkl in pklpaths:
+    with open('../temp/'+ pkl, 'rb') as f:
+        p = pickle.load(f)
+        wb = openpyxl.load_workbook(template)
+        sh = wb['weekly']
 
-for q, rows in fields.items():
-        if type(rows) == int:
-            cells = [column + str(rows)]
-            names = [name_col + str(rows)]
-        else:
-            cells = [column+str(r) for r in rows]
-            names = [name_col+str(r) for r in rows]
-        for name, cnum in zip(names, cells):
-            try:
-                res = p['q'+str(q)]
-                print(sh[name].value, res[0])
-                if sh[name].value == res[0]:
-                    print(f"name {name} and cell {cell} match")
-            except IndexError:
-                print(f"No entry for query {q} or res at {i} for {q} nonexistent")
-#18,19,20,27,59
-sh.columns('E')
+    for q, entry in fields.items():
+        if type(entry) == int:
+            #then one-row query
+            shloc = column+str(entry)
+            write_val = p[q][0][0] #eg {'q1':[(100,)]}
+            sh[shloc] = write_val
+        else: #then field is a dict of name, nrow pairs
+            results = p[q] #eg [('APP下载', 273764),('微信卡包', 367695),('微信小程序', 3420), etc
+            for name, datum in results: #name, value tuple
+                nrow = entry[name]
+                shloc = column + str(nrow)
+                sh[shloc] = datum
+
+    wb.save(f'../temp/{pkl[:-4]}.xlsx')
